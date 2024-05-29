ef8b3dcc95a4f0c341c7452cb3cff92145645bbe
Roman <romatalalaj41@gmail.com>
2024-05-29 10:13:25 +0300

    Last fix

--- cypress/e2e/seo-tests/textBlockTest.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Перевірка текстового блоку', () => {
-    it('Коректне відображення текстового блоку', () => {
-        cy.visit('/');
-        cy.get('.text-block').should('be.visible').and('not.be.empty');
-    });
-});
